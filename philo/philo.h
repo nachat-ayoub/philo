f41a3077393e38c3738605eb2c06bef5f838742f
dev-mint <nachatayoub68@gmail.com>
2025-03-19 21:58:10 +0000

    add struct's

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: anachat <anachat@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 16:10:59 by anachat           #+#    #+#             */
-/*   Updated: 2025/03/19 14:30:08 by anachat          ###   ########.fr       */
+/*   Updated: 2025/03/19 21:57:57 by anachat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,22 @@
 # include <stdio.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <pthread.h>
+
+typedef struct s_fork
+{
+	int fork_id;
+	pthread_mutex_t fork;
+}	t_fork;
+
+typedef struct s_philo
+{
+	int		id;
+	long	meals_c;
+	long	last_eat;
+	int		is_full;
+	t_fork	*l_fork;	
+	t_fork	*r_fork;	
+}	t_philo;
 
 #endif
