d303752979ae52c9fe6e0d8332c0925bb4304946
dev-mint <nachatayoub68@gmail.com>
2025-03-19 14:54:19 +0000

    setup project

--- philo/philo.c
+++ philo/philo.c
@@ -6,13 +6,13 @@
 /*   By: anachat <anachat@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 16:07:14 by anachat           #+#    #+#             */
-/*   Updated: 2025/03/18 17:51:32 by anachat          ###   ########.fr       */
+/*   Updated: 2025/03/19 14:34:23 by anachat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
 	printf("ac: %d\n", ac);
 	printf("av: %s\n", av[0]);
--- philo_bonus/philo_bonus.h
+++ philo_bonus/philo_bonus.h
@@ -1,13 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils.c                                            :+:      :+:    :+:   */
+/*   philo_bonus.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anachat <anachat@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/18 17:45:49 by anachat           #+#    #+#             */
-/*   Updated: 2025/03/19 14:30:06 by anachat          ###   ########.fr       */
+/*   Created: 2025/03/18 16:10:59 by anachat           #+#    #+#             */
+/*   Updated: 2025/03/18 17:53:35 by anachat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo.h"
+#ifndef PHILO_BONUS_H
+# define PHILO_BONUS_H
+
+# include <stdio.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+#endif
